634e97e0fa078abfe09cdf5e967b88b7ea3c2c90
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-07-10 19:23:37 +0800

    Added ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,64 +2,74 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Requests\ProductRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        return Product::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+
+        $validated = $request->validated() ;
+        $validated['image_path'] = $request->file('image_path')->storePublicly('product', 'public');
+        $validated['user_id'] = $request->user()->id;
+
+       Product::create($validated);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show( string $id )
     {
-        //
+        Product::findorFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(ProductRequest $request,string $id)
     {
-        //
+         $validated = $request->validated();
+         $item = Product::findOrFail($id);
+
+           // If new image is uploaded
+    if ($request->hasFile('image_path')) {
+        // Delete old image
+        if (!is_null($item->image_path)) {
+            Storage::disk('public')->delete($item->image_path);
+        }
+
+        // Store new image and update in validated data
+        $validated['image_path'] = $request->file('image_path')->store('product', 'public');
+    }
+
+      // Update item with all validated data (including new image_path if applicable)
+        Product::update($validated);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(string $id)
     {
-        //
+        Product::findOrFail($id);
     }
 }
